b40fc59aeabc82b8fef7ad9e1e0904164f9023f3
swapna <swapna.akuthota@apisero.com>
2020-12-22 11:48:21 +0530

    i am the one

---
+++ matrix.java
@@ -0,0 +1,15 @@
+public class matrix {
+
+	public static void main(String[] args) {
+	
+		int xyz[]= {10,20,30,40,50};
+		int sum = 0;
+		{
+			for(int i=0;i<xyz.length;i++)
+				sum = sum + xyz[i];
+		}
+		
+			System.out.println(sum);
+		}
+       
+	}
